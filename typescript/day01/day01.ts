4a6041c1a2e106e37a7d63d332c1f2dc21611789
Nicholas Digati <ndigati@gmail.com>
2021-12-04 19:01:24 -0800

    Add day01 and day02 in typescript

---
+++ typescript/day01/day01.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function readInput(filename: string): number[] {
+    let file = fs.readFileSync(filename, "utf-8")
+    return file.split("\n").map((line) => parseInt(line))
+}
+
+function getTriples(input: number[]): number[] {
+    let result: number[] = [];
+    input.forEach((value, index) => {
+        result.push(value+input[index+1]+input[index+2]);
+    })
+    return result
+}
+
+function getIncreases(input: number[]): number {
+    let prev = Number.MAX_SAFE_INTEGER;
+    let increases = 0;
+    input.forEach((value) => {
+        if (value > prev) {
+            increases++
+        }
+        prev = value
+    });
+    return increases
+}
+
+function main() {
+    const filename = path.join(__dirname, 'input.txt');
+    const input = readInput(filename);
+
+    console.log("Part 1: " + getIncreases(input));
+
+    const triples = getTriples(input)
+    console.log("Part 2: " + getIncreases(triples))
+}
+
+main()
---
+++ typescript/day02/day02.ts
@@ -0,0 +1,72 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function readInput(filename: string): Command[] {
+    let file = fs.readFileSync(filename, "utf-8")
+    let commands: Command[] = [];
+
+    file.split("\n").forEach((value) => {
+        let [direction, unit] = value.split(' ');
+        commands.push({direction: direction, units: parseInt(unit)})
+    });
+
+    return commands
+}
+
+interface Command {
+    direction: string,
+    units: number
+}
+
+interface Position {
+    horizontal: number,
+    depth: number,
+    aim: number
+}
+
+function calculateFinalPosition(input: Command[], withAim: boolean): Position {
+    const finalPosition: Position = { horizontal: 0, depth: 0, aim: 0};
+
+    for (const command of input) {
+        switch (command.direction) {
+            case "forward":
+                if (withAim) {
+                    finalPosition.horizontal += command.units;
+                    finalPosition.depth += (command.units * finalPosition.aim)
+                } else {
+                    finalPosition.horizontal += command.units;
+                }
+                break;
+            case "down":
+                if (withAim) {
+                    finalPosition.aim += command.units;
+                } else {
+                    finalPosition.depth += command.units;
+                }
+                break;
+            case "up":
+                if (withAim) {
+                    finalPosition.aim -= command.units;
+                } else {
+                    finalPosition.depth -= command.units;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    return finalPosition
+}
+
+function main() {
+    const filename = path.join(__dirname, 'input.txt');
+    const input = readInput(filename);
+
+    const part1 = calculateFinalPosition(input, false);
+    const part2 = calculateFinalPosition(input, true);
+    console.log("Part 1: " + (part1.depth*part1.horizontal));
+    console.log("Part 2: " + (part2.depth*part2.horizontal));
+}
+
+main()
